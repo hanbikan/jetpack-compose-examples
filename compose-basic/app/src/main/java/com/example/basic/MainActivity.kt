16bc964bec11db188bc733b58f1332f2969ea2a1
강한빛 <fchopinof99@gmail.com>
2022-06-22 11:05:56 +0900

    Add basic use for Column, Row, Text

---
+++ compose-basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.basic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.basic.ui.theme.JetpackBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackBasicTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = Color.Blue)
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text("Hello")
+                        Text("World")
+                    }
+                }
+            }
+        }
+    }
+}
