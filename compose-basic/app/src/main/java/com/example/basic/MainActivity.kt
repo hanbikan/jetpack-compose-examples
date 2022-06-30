1f2e026d482cc36356000be97c5b50d4f03018b0
Hanbit-Kang <fchopinof99@gmail.com>
2022-06-30 16:53:07 +0900

    Add Image, Card, State example

--- compose-basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ compose-basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -3,18 +3,28 @@ package com.example.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basic.ui.theme.JetpackBasicTheme
@@ -23,21 +33,56 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
+            }
+            ImageCard(
                 modifier = Modifier
-                    .background(color = Color.Red)
-                    .fillMaxWidth(),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp),
+                isFavorite = isFavorite
             ) {
-                item {
-                    Text("Header")
-                }
-                items(50) { index ->
-                    Text("글자 $index")
-                }
-                item {
-                    Text("Footer")
+                isFavorite = it
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onClick: (Boolean) -> Unit
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
+    ) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.tp_in_rect),
+                contentDescription = "An example",
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                IconButton(onClick = {
+                    onClick.invoke(!isFavorite)
+                }) {
+                    Icon(
+                        imageVector =
+                            if (isFavorite) Icons.Default.Favorite
+                            else Icons.Default.FavoriteBorder,
+                        contentDescription = "FavoriteBorder",
+                        tint = Color.White
+                    )
                 }
             }
         }
