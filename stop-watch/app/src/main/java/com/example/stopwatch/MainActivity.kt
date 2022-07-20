4efb85e43188ff7659ec270b90719ffed594e3a2
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-20 19:47:41 +0900

    Implement MainViewModel

--- stop-watch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ stop-watch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -8,36 +8,71 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.example.stopwatch.ui.theme.StopWatchTheme
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StopWatchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+class MainViewModel: ViewModel() {
+    private var time = 0
+    private var timerTask: Timer? = null
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    StopWatchTheme {
-        Greeting("Android")
+    private val _isRunning = mutableStateOf(false)
+    val isRunning: State<Boolean> = _isRunning
+
+    private val _second = mutableStateOf(0)
+    val second: State<Int> = _second
+
+    private val _millisecond = mutableStateOf(0)
+    val millisecond: State<Int> = _millisecond
+
+    private val _lapTimes = mutableStateOf(mutableListOf<String>())
+    val lapTimes: State<List<String>> = _lapTimes
+
+    private var lapCount = 1
+
+    fun start() {
+        _isRunning.value = true
+
+        timerTask = timer(period = 10) {
+            time++
+            _second.value = time / 1000
+            _millisecond.value = time % 1000
+        }
+    }
+
+    fun pause() {
+        _isRunning.value = false
+        timerTask?.cancel()
+    }
+
+    fun reset() {
+        timerTask?.cancel()
+
+        time = 0
+        _isRunning.value = false
+        _second.value = 0
+        _millisecond.value = 0
+
+        _lapTimes.value.clear()
+        lapCount = 1
+    }
+
+    fun recordLapTime() {
+        _lapTimes.value.add(0, "$lapCount LAP : ${second.value}.${millisecond.value}")
+        lapCount++
     }
 }
