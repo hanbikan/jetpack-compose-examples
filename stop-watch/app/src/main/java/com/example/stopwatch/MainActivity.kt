b286f2bb3e3cb2ac23689023cbd3cb4b32bee5ac
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-20 20:00:09 +0900

    Implement MainScreen()

--- stop-watch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ stop-watch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.stopwatch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.example.stopwatch.ui.theme.StopWatchTheme
 import java.util.*
@@ -74,5 +80,83 @@ class MainViewModel: ViewModel() {
     fun recordLapTime() {
         _lapTimes.value.add(0, "$lapCount LAP : ${second.value}.${millisecond.value}")
         lapCount++
+    }
+}
+
+@Composable
+fun MainScreen(
+    second: Int,
+    millisecond: Int,
+    isRunning: Boolean,
+    lapTimes: List<String>,
+    onReset: () -> Unit,
+    onToggle: (Boolean) -> Unit,
+    onLapTime: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("StopWatch") })
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row(
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Text("$second", fontSize = 100.sp)
+                Text("$millisecond")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                lapTimes.forEach { lapTime ->
+                    Text(lapTime)
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                FloatingActionButton(
+                    onClick = onReset,
+                    backgroundColor = Color.Red
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_baseline_refresh_24),
+                        contentDescription = "reset"
+                    )
+                }
+
+                FloatingActionButton(
+                    onClick = { onToggle(isRunning) },
+                    backgroundColor = Color.Green
+                ) {
+                    Image(
+                        painter = painterResource(
+                            id = if(isRunning) R.drawable.ic_baseline_pause_24
+                            else R.drawable.ic_baseline_play_arrow_24
+                        ),
+                        contentDescription = "toggle"
+                    )
+                }
+
+                Button(onClick = onLapTime) {
+                    Text("랩 타임")
+                }
+            }
+        }
     }
 }
