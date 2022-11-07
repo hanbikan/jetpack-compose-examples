34cc729d66105f3027a3063880cb563d67c1a9fe
Hanbit-Kang <fchopinof99@gmail.com>
2022-11-07 22:26:12 +0900

    Modify PokemonCard design

--- list-and-detail-view/app/src/main/java/com/hanbitkang/listanddetailview/MainActivity.kt
+++ list-and-detail-view/app/src/main/java/com/hanbitkang/listanddetailview/MainActivity.kt
@@ -3,16 +3,17 @@ package com.hanbitkang.listanddetailview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.hanbitkang.listanddetailview.model.Pokemon
 import com.hanbitkang.listanddetailview.ui.theme.ListAndDetailViewTheme
@@ -27,11 +28,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val pokemons = listOf(
-                        Pokemon("https://pokeapi.co/api/v2/pokemon/1/", "first"),
-                        Pokemon("https://pokeapi.co/api/v2/pokemon/2/", "second")
-                    )
-                    PokemonList(pokemons)
+                    val pokemonList = (1 until 20)
+                        .map { Pokemon("https://pokeapi.co/api/v2/pokemon/$it/", "Pokemon$it") }
+                    PokemonList(pokemonList)
                 }
             }
         }
@@ -49,9 +48,16 @@ fun PokemonList(pokemonList: List<Pokemon>) {
 
 @Composable
 fun PokemonCard(pokemon: Pokemon) {
-    Row {
+    Row(
+        modifier = Modifier.padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         PokemonImage(pokemonImageUrl = pokemon.getImageUrl())
-        Text(text = pokemon.name?: "")
+        Text(
+            text = pokemon.name?: "",
+            fontSize = 32.sp,
+            modifier = Modifier.padding(start = 16.dp)
+        )
     }
 }
 
@@ -59,6 +65,9 @@ fun PokemonCard(pokemon: Pokemon) {
 fun PokemonImage(pokemonImageUrl: String?) {
     AsyncImage(
         model = pokemonImageUrl,
-        contentDescription = null
+        contentDescription = null,
+        modifier = Modifier
+            .width(96.dp)
+            .height(96.dp)
     )
 }
